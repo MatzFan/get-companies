5c9d154cc304853d5dee77eab9614321816c921f
Bruce Steedman <bruce.steedman@gmail.com>
2014-09-05 18:25:02 +0100

    Change route to display company_data

--- web.rb
+++ web.rb
@@ -2,5 +2,5 @@ require 'sinatra'
 require_relative './scraper'
 
 get '/' do
-  Scraper.new(params[:text]).companies.map { |row| row.join('|') }.join("\n")
+  Scraper.new(params[:text]).company_data.map { |row| row.join('|') }.join("\n")
 end
