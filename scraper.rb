805207de34da451a02da9a8a37d50ab08884ccee
Bruce Steedman <bruce.steedman@gmail.com>
2014-09-05 11:18:52 +0100

    Add page_source method, tested with search text "ab"

--- scraper.rb
+++ scraper.rb
@@ -3,10 +3,14 @@ require 'mechanize'
 class Scraper
 
   URL = 'https://www.jerseyfsc.org/registry/documentsearch/'
+  BEGINS = 'BeginsWith'
+  SEARCH_TYPE_FIELD = 'ctl00$Main$searchTypesComboBox'
+  SEARCH_TEXT_FIELD = 'ctl00$Main$txtSearch'
 
-  attr_reader :search_text
+  attr_reader :agent, :search_text
 
   def initialize(search_text)
+    @agent = Mechanize.new
     @search_text = search_text
   end
 
@@ -14,4 +18,14 @@ class Scraper
     []
   end
 
+  def page_source
+    home_page = @agent.get(URL)
+    form = home_page.form
+    form.send(SEARCH_TYPE_FIELD, BEGINS)
+    form.send(SEARCH_TEXT_FIELD, search_text) # ditto
+    @agent.submit(form, form.buttons.first).body
+  end
+
 end
+
+# puts Scraper.new('ab').page_source.body
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -4,6 +4,12 @@ describe Scraper do
   let(:ab) {'ab'}
   let(:scraper) { Scraper.new(ab) }
 
+  context "#page_source" do
+    specify "should return page source for Scraper's search text" do
+      expect(scraper.page_source).to include('46541">ABACUS INVESTMENTS')
+    end
+  end
+
   context "#get_companies" do
     specify "should return a list of companies starting with 'ab'" do
       expect(scraper.get_companies).to eq([])
